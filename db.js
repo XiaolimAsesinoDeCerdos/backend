4a3f6aa9a2f4349197b58fa5033c0a4d3bcc5a2b
Robert1532 <apr0034341@est.univalle.edu>
2026-02-12 21:50:32 -0400

    actuali

--- db.js
+++ db.js
@@ -1,17 +1,30 @@
 // db.js
 const mysql = require("mysql2");
 
+const host = process.env.MYSQLHOST;
+const port = Number(process.env.MYSQLPORT || 3306);
+const user = process.env.MYSQLUSER;
+const database = process.env.MYSQLDATABASE;
+
+console.log("[DB] host:", host);
+console.log("[DB] port:", port);
+console.log("[DB] user:", user);
+console.log("[DB] database:", database);
+
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  port: Number(process.env.MYSQLPORT ),
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+    host,
+    port,
+    user,
+    password: process.env.MYSQLPASSWORD,
+    database,
 });
 
 db.connect((err) => {
-  if (err) console.error("Error BD:", err.message);
-  else console.log("✓ BD conectada correctamente");
+    if (err) {
+        console.error("Error BD (full):", err); // <- importante
+    } else {
+        console.log("✓ BD conectada correctamente");
+    }
 });
 
 module.exports = db;
